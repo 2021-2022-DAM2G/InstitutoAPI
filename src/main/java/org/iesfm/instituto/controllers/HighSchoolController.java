6dd28a726d76ed221d52b22eba897de4faa58344
migsamu <mufrikiler@gmail.com>
2021-05-28 10:23:25 +0200

    controlador Student, metodo delete, falta el resto

--- src/main/java/org/iesfm/instituto/controllers/HighSchoolController.java
+++ src/main/java/org/iesfm/instituto/controllers/HighSchoolController.java
@@ -1,4 +1,4 @@
-package org.iesfm.instituto;
+package org.iesfm.instituto.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
---
+++ src/main/java/org/iesfm/instituto/controllers/StudentController.java
@@ -0,0 +1,25 @@
+package org.iesfm.instituto.controllers;
+
+import org.iesfm.instituto.Student;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/student/{}")
+    public List<Student> getStudents() {
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/student/{nif}")
+    public void deleteEstudent(@PathVariable("nif") String nif){
+        System.out.println("Eliminado!");
+    }
+
+
+}
